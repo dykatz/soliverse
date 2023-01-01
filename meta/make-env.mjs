ec690b388684ceffe0a39609bdc8cd3e9cfeb44a
Dylan Katz <dylanjaredkatz@gmail.com>
2022-12-31 16:20:32 -0800

    Meta: Set AUTH_URL in make-env

--- meta/make-env.mjs
+++ meta/make-env.mjs
@@ -1,8 +1,10 @@
 // @ts-check
+import { randomBytes } from 'crypto'
 import { readFileSync, writeFileSync } from 'fs'
 
-const data = readFileSync('.env-example', 'utf8')
+const template = readFileSync('.env-example', 'utf8')
 
-// ...
+const authSecret = randomBytes(16).toString('hex')
+const data = template.replace('%AUTH_SECRET%', authSecret)
 
 writeFileSync('.env', data)
